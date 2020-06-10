92c5a73ef9775fa5b3d92631bd161ee93aeb7b10
Rick <rick@gnous.eu>
2020-06-10 10:48:38 +0200

    Ajout doc et chemin requirements.txt Le requirements se fait maintenant dans le chemin entré

--- autoRequirements.sh
+++ autoRequirements.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+
+#**********************************#
+#        autoRequirements.sh       #
+#                                  #
+#       author: rick@gnous.eu      #     
+#           licence: MIT           #
+#**********************************#
+
 path=${1:-.}
 
 INDEX=0
 installedPackage=($(pip3 freeze))
 packagePutInReq=()
 
+# ========================================================================= # 
+# contains()                                                                #
+# Vérifie si le packet est dans la liste des packets installés              #
+#                                                                           #
+# Utilise la variable globale installedPackage.                             #
+#                                                                           #
+# Paramètres : $1 le packet à vérifier                                      #
+#                                                                           #
+# Retourne : - 0 si le packet est dans la liste des packets installés       #
+#              Mets à jour la variable globale INDEX avec l'index du packet #
+#              dans la tableau installedPackagei                            #
+#            - 1 sinon                                                      #
+# ========================================================================= # 
 contains() {
     value=$1
     i=0
@@ -20,6 +41,18 @@ contains() {
     return 1
 }
 
+# ====================================================================== # 
+# checkContains()                                                        #
+# Vérifie si le packet n'est pas déjà présent dans la liste des packets  #
+# qui seront dans le requirements.txt                                    #
+#                                                                        #
+# Utilise la variable globale packagePutInReq.                           #
+#                                                                        #
+# Paramètres : $1 le packet à vérifier                                   #
+#                                                                        #
+# Retourne : - 0 si le packet est déjà présent dans la liste des packets #
+#            - 1 sinon                                                   #
+# ====================================================================== # 
 checkContains() {
     value=$1
     for element in "${packagePutInReq[@]}"; do
@@ -30,23 +63,28 @@ checkContains() {
     return 1
 }
 
+echo 'Récupération des packets à installés...'
 for file in $(find $path -iname "*.py"); do
+    # boucle sur les lines qui commencent par import ou from. Vous pouvez voir la commande utilisée
+    # à la fin de la boucle
     while read -r line; do
         package=$(cut -d' ' -f2 <<< $line)
         contains $package 
         if [ $? -eq 0 ]; then
             newPackage=${installedPackage[$INDEX]}
-            echo $newPackage
             checkContains $newPackage
             if [ $? -eq 1 ]; then
                 packagePutInReq+=($newPackage)
             fi
-            echo "${packagePutInReq[@]}"
         fi
     done < <(grep '\(^[[:blank:]]*import\)\|\(^[[:blank:]]*from\)' $file)
 done
+echo 'Fait.'
 
-touch 'requirements.txt'
+echo 'Création et insertion des packets dans requirements.txt...'
+pathRequirement=$path'/requirements.txt'
+touch pathRequirement
 for package in "${packagePutInReq[@]}"; do
-    echo $package >> 'requirements.txt'
+    echo $package >> $pathRequirement
 done
+echo 'Fait.'
